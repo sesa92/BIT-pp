0848af114db6fff3e5a24c13b260e6f20ad83db3
sesa92 <sesa992@gmail.com>
2021-03-16 21:03:25 +0100

    inital commit

--- loops_exercise.js
+++ loops_exercise.js
@@ -77,7 +77,23 @@ for (var i = 0; i <= 20; i++) {
 console.log(sum);
 
 
-// 7.
+// 7. Write a program that computes average marks of the following students. Then use this average to determine the corresponding grade.
+//   David 80
+//   Marko 77
+//   Dany 88
+//   John 95
+//   Thomas 68
+//
+// The grades are computed as follows :
+//
+//  <60%    F
+//  <70%    D
+//  <80%    C
+//  <90%    B
+//  <100%   A
+
+
+
 
 var students = [['David', 80], ['Marko', 77], ['Dany', 88], ['John', 95], ['Thomas', 68]];
 var sum = 0;
